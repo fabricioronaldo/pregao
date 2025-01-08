2261c8a0341f220c50f348d19eff235f3ddf42b4
Fabricio Velasco <fabriciorvelasco@gmail.com>
2025-01-08 07:15:26 -0300

    Criação Delete Plataforma e criação Status

--- notice/urls.py
+++ notice/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from notice.views import index
 from django.conf import settings
 from django.conf.urls.static import static
-from notice.views import NewPlatformCreateView, NewStatusCreateView, PlatformListView, PlatformUpdateView
+from notice.views import NewPlatformCreateView, NewStatusCreateView, PlatformListView, PlatformUpdateView, PlatformDeleteView, StatusListView,StatusDeteleView, StatusUpdateView
 
 urlpatterns = [
     path('', index, name='index'),
@@ -11,5 +11,9 @@ urlpatterns = [
     path('new_status/', NewStatusCreateView.as_view(), name='new_status'),
     path('platform/', PlatformListView.as_view(), name='platforms' ),
     path('platform/<int:pk>/update', PlatformUpdateView.as_view(), name='platform_update'),
+    path('platform/<int:pk>/delete', PlatformDeleteView.as_view(), name='platform_delete'),
+    path('status/', StatusListView.as_view(), name='status' ),
+    path('status/<int:pk>/update', StatusUpdateView.as_view(), name='status_update'),
+    path('status/<int:pk>/delete', StatusDeteleView.as_view(), name='status_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
--- notice/views.py
+++ notice/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render,redirect
 from notice.models import Platform, Status
 from notice.forms import PlatformModelForm, StatusModelForm
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
-
-class PlatformUpdateView(UpdateView):
-  model = Platform
-  form_class = PlatformModelForm
-  template_name = 'notice/platform_update.html'
-  success_url = '/'
+from django.urls import reverse_lazy
+# Views da Plataforma
 
 class NewPlatformCreateView(CreateView):
   model = Platform
@@ -15,18 +11,45 @@ class NewPlatformCreateView(CreateView):
   template_name = 'cadastro/new_platform.html'
   success_url = '/'
 
+class PlatformUpdateView(UpdateView):
+  model = Platform
+  form_class = PlatformModelForm
+  template_name = 'notice/platform_update.html'
+  success_url = '/'
+
+class PlatformDeleteView(DeleteView):
+  model = Platform
+  template_name = 'notice/platform_delete.html'
+  success_url = '/'
+  
+class PlatformListView(ListView):
+  model = Platform
+  template_name = 'notice/platform.html'
+  context_object_name = 'platforms'
+  
+# Views do Status
+
+class StatusListView(ListView):
+  model = Status
+  template_name = 'notice/status.html'
+  context_object_name =  'status' 
+
 class NewStatusCreateView(CreateView):
   model = Status
   form_class = StatusModelForm
   template_name = 'cadastro/new_status.html'
   success_url = '/'
 
-class PlatformListView(ListView):
-  model = Platform
-  template_name = 'notice/platform.html'
-  context_object_name = 'platforms'
-  
-  
-  
+class StatusUpdateView(UpdateView):
+  model = Status
+  form_class = StatusModelForm
+  template_name = 'notice/status_update.html'
+  success_url = reverse_lazy('status')  
+
+class StatusDeteleView(DeleteView):
+  model = Status
+  template_name = 'notice/status_delete.html'
+  success_url = reverse_lazy('status')
+    
 def index(request):
   return render(request, 'notice/index.html')
